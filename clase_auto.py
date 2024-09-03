3af5184d43f9e75ecadf0f2553856a8ecaa9211a
rodrigo140806 <202410466@urp.edu.pe>
2024-09-03 18:25:24 +0000

    Actualizado 03/09/2024

---
+++ clase_auto.py
@@ -0,0 +1,31 @@
+# Simula el funcionamiento de un auto
+class Auto:
+    # Función para definir el constructor
+    def __init__(self, marca, disp_gasolina):
+        self.marca = marca
+        self.disp_gasolina = disp_gasolina
+
+    # Definir los métodos
+    def avanzar(self, km):
+        self.disp_gasolina -= km / 40
+
+    def retroceder(self, km):
+        self.disp_gasolina -= km / 20
+
+# Crear objetos
+auto1 = Auto("Toyota", 15)
+auto2 = Auto("Ford", 10)
+
+print("Auto1: " + auto1.marca + ", Gas: " + str(auto1.disp_gasolina))
+print("Auto2: " + auto2.marca + ", Gas: " + str(auto2.disp_gasolina))
+
+# Recorrer los autos
+auto1.avanzar(120)
+auto1.retroceder(10)
+
+auto2.avanzar(160)
+auto2.retroceder(40)
+
+print("Auto1: " + auto1.marca + ", Gas: " + str(auto1.disp_gasolina))
+print("Auto2: " + auto2.marca + ", Gas: " + str(auto2.disp_gasolina))
+
